d13a7912a9b5fa01ad2265525cac554c403834aa
Ning Mei <semsociaty@gmail.com>
2019-08-23 15:34:58 +0200

    Add files via upload

--- scripts/EEG/RNN decoding test 1.py
+++ scripts/EEG/RNN decoding test 1.py
@@ -15,138 +15,40 @@ from glob import glob
 from shutil import copyfile
 copyfile('../utils.py','utils.py')
 from utils import get_frames
+from scipy.stats import mode
 from sklearn import metrics
 from sklearn.utils import shuffle
 from sklearn.preprocessing import MinMaxScaler,StandardScaler
 from sklearn.model_selection import train_test_split,StratifiedShuffleSplit
 import tensorflow as tf
-from keras import layers,Model, optimizers,losses,regularizers,callbacks#,Sequential
-from keras import metrics as k_metrics
-import keras.backend as K
+from tensorflow.keras import layers,Model, optimizers,losses,regularizers,callbacks#,Sequential
+from tensorflow.keras import metrics as k_metrics
+import tensorflow.keras.backend as K
+from collections import Counter
 
-def make_CallBackList(model_name,monitor='val_loss',mode='min',verbose=0,min_delta=1e-4,patience=50,frequency = 1):
-    from keras.callbacks import ModelCheckpoint,EarlyStopping
-    """
-    Make call back function lists for the keras models
-    
-    Inputs
-    -------------------------
-    model_name: directory of where we want to save the model and its name
-    monitor: the criterion we used for saving or stopping the model
-    mode: min --> lower the better, max --> higher the better
-    verboser: printout the monitoring messages
-    min_delta: minimum change for early stopping
-    patience: temporal windows of the minimum change monitoring
-    frequency: temporal window steps of the minimum change monitoring
-    
-    Return
-    --------------------------
-    CheckPoint: saving the best model
-    EarlyStopping: early stoppi....
-    """
-    checkPoint = ModelCheckpoint(model_name,# saving path
-                                 monitor          = monitor,# saving criterion
-                                 save_best_only   = True,# save only the best model
-                                 mode             = mode,# saving criterion
-                                 period           = frequency,# frequency of check the update 
-                                 verbose          = verbose# print out (>1) or not (0)
-                                 )
-    earlyStop = EarlyStopping(   monitor          = monitor,
-                                 min_delta        = min_delta,
-                                 patience         = patience,
-                                 verbose          = verbose, 
-                                 mode             = mode,
-                                 )
-    return [checkPoint,earlyStop]
-def preprocess_features(X,vectorizer = None,scaler = None):
-    if vectorizer is None:
-        vectorizer          = mne.decoding.Vectorizer()
-        X_vec               = vectorizer.fit_transform(X)
-    else:
-        X_vec               = vectorizer.transform(X)
-    if scaler is None:
-        scaler              = StandardScaler()
-        X_vec_standard      = scaler.fit_transform(X_vec)
-    else:
-        X_vec_standard      = scaler.transform(X_vec)
-    X_vec_tran          = vectorizer.inverse_transform(X_vec_standard)
-    
-    return X_vec_tran,vectorizer,scaler
+import utils_deep
 
-#def call_back_dict(classifier,temp,care = 'loss'):
-#    if care == 'loss':
-#        c = (temp[0] + 1) * 1e-3
-#        print(f'criterion = {c:.4f}')
-#        return dict(
-#                monitor = 'val_{}'.format(classifier.metrics_names[0]),
-#                mode = 'min',
-#                patience = 1,
-#                min_delta = c)
-#    elif care == 'metric':
-#        c = (temp[1] + 1) * 1e-4
-#        print(f'criterion = {c:.4f}')
-#        return dict(
-#                monitor = 'val_{}'.format(classifier.metrics_names[-1]),
-#                mode = 'max',
-#                patience = 1,
-#                min_delta = c)
-#    else:
-#        print('why?')
-
-def build_model(timesteps,data_dim,n_units = 1,batch_size = 10,n_layers = 3,drop = True):
-    K.clear_session()
-    inputs      = layers.Input(
-                       shape = (timesteps,data_dim,),
-                       batch_shape = (batch_size,timesteps,data_dim),
-                       name = 'inputs')
-    inputs_ = inputs
-    RNN,state_h = layers.GRU(units = n_units,
-                             return_state = True,
-                             return_sequences = True,
-                             name = 'rnn{}'.format(1))(inputs_)
-    for n_temp in range(n_layers - 1):
-        RNN,state_h = layers.GRU(units = n_units,
-                                 return_state = True,
-                                 return_sequences = True,
-                                 dropout = 0.1,
-                                 recurrent_dropout = 0.25,
-                                 name = 'rnn{}'.format(n_temp + 2))(RNN,initial_state = [state_h])
-        if drop:
-            RNN = layers.Dropout(0.25,name = 'drop{}'.format(n_temp + 1))(RNN)
-    RNN = layers.GlobalAveragePooling1D(data_format = 'channels_first',name = 'average')(RNN) #TODO: what is the rational of this?
-    outputs = layers.Dense(2,activation = 'softmax',
-                           activity_regularizer = regularizers.l1(1),
-                           name = 'outputs',)(RNN)
-    classifier = Model(inputs,outputs,name = 'clf')
-    return classifier
-
-all_subjects = [
-                'matie_5_23_2019',
-                'pedro_5_14_2019',
-                'aingere_5_16_2019',
-                'inaki_5_9_2019',
+all_subjects = ['aingere_5_16_2019',
+                'alba_6_10_2019',
+                'alvaro_5_16_2019',
                 'clara_5_22_2019',
                 'ana_5_21_2019',
+                'inaki_5_9_2019',
+                'jesica_6_7_2019',
                 'leyre_5_13_2019',
                 'lierni_5_20_2019',
-                'xabier_5_15_2019',
                 'maria_6_5_2019',
-                'jesica_6_7_2019',
+                'matie_5_23_2019',
+                'out_7_19_2019',
+                'mattin_7_12_2019',
+                'pedro_5_14_2019',
+                'xabier_5_15_2019',
                 ]
-all_subjects = np.sort(all_subjects)
+all_subjects = np.sort(all_subjects[1:])
 
 for subject in all_subjects:
-    working_dir         = f'../../data/clean EEG/{subject}'
+    working_dir         = f'../../data/clean EEG highpass detrend/{subject}'
     working_data        = glob(os.path.join(working_dir,'*-epo.fif'))
-    # there was a bug in the csv file, so the early behavioral is treated differently
-    date                = '/'.join(re.findall(r'\d+',subject))
-    date                = datetime.strptime(date,'%m/%d/%Y')
-    breakPoint          = datetime(2019,3,10)
-    if date > breakPoint:
-        new             = True
-    else:
-        new             = False
-    frames,_            = get_frames(directory = f'../../data/behavioral/{subject}',new = new)
     n_splits            = 25
     n_epochs            = int(2e2)
     print_model         = True
@@ -174,97 +76,64 @@ for subject in all_subjects:
             
             X_,y_               = epochs.get_data(),epochs.events[:,-1]
             y_                  = y_ //100 - 2
-            
-            
             X,targets           = X_.copy(),y_.copy()
             targets             = np.vstack([targets,1-targets]).T
             
             X                   = mne.decoding.Scaler(epochs.info).fit_transform(X)
-            
+            # because RNN take input's last dimension as the feature while the second the last dimension as time step
             X                   = np.swapaxes(X,1,2)
             ss                  = []
             cv                  = StratifiedShuffleSplit(n_splits=n_splits,test_size = 0.15,random_state=12345)
             for fold,(idx_,idx_test) in enumerate(cv.split(X,targets)):
                 
                 X_train,X_valid,y_train,y_valid = train_test_split(
-                        X[idx_],targets[idx_],
-                        test_size               = 0.15,
-                        random_state            = 12345,
-                        shuffle                 = True,)
-                
-#                X_train,vectorizer,scaler = preprocess_features(X_train)
-#                X_valid,_,_ = preprocess_features(X_valid,vectorizer,scaler)
-                X_train = X_train / np.abs(X_train.max())
-                X_valid = X_valid / np.abs(X_valid.max())
-                
+                                    X[idx_],targets[idx_],
+                                    test_size           = 0.15,
+                                    random_state        = 12345,
+                                    shuffle             = True,)
                 
                 model_name  = 'RNN.hdf5'
                 batch_size  = 10
                 timesteps   = X.shape[1]
                 data_dim    = X.shape[2]
-                n_units     = 10
+                n_units     = 1
                 n_layers    = 1
                 dropout     = True
-                
-                remain_train = X_train.shape[0] % batch_size
-                remain_valid = X_valid.shape[0] % batch_size
-                
-                if remain_train != 0:
-                    np.random.seed(12345)
-                    idx_train = np.random.choice(X_train.shape[0],size = X_train.shape[0] - remain_train)
-                    X_train,y_train = X_train[idx_train],y_train[idx_train]
-                
-                if remain_valid != 0:
-                    np.random.seed(12345)
-                    idx_valid = np.random.choice(X_valid.shape[0],size = X_valid.shape[0] - remain_valid)
-                    X_valid,y_valid = X_valid[idx_valid],y_valid[idx_valid]
-                
-                from collections import Counter
-                class_weight = dict(Counter(y_train[:,0]))
-                class_weight = {key:(y_train.shape[0] - value)/value for key,value in class_weight.items()}
-                print(class_weight)
-                sample_weight = [class_weight[item] for item in y_train[:,0]]
-                
-                classifier = build_model(timesteps=timesteps,
-                                         data_dim=data_dim,
-                                         n_units=n_units,
-                                         batch_size=batch_size,
-                                         n_layers = n_layers,
-                                         drop=dropout)
+                # make the model
+                classifier = utils_deep.build_model(timesteps   = timesteps,
+                                                    data_dim    = data_dim,
+                                                    n_units     = n_units,
+                                                    batch_size  = batch_size,
+                                                    n_layers    = n_layers,
+                                                    drop        = dropout)
                 print(f'the model has {classifier.count_params()} parameters')
                 if print_model:
                     classifier.summary()
                     print_model = False
+                    
+                # compile the model with optimizer, loss function
                 classifier.compile(optimizer                = optimizers.Adam(lr = 1e-4),
                                    loss                     = losses.categorical_crossentropy,
                                    metrics                  = [k_metrics.categorical_accuracy])
+                # early stopping
+                callBackList = utils_deep.make_CallBackList(
+                                   model_name,
+                                   verbose                  = 0,# print out the process
+                                   frequency                = 1,
+                                   **utils_deep.call_back_dict(classifier,'loss'))
                 
-                temp = [1,5]
-                def call_back_dict(care = 'loss'):
-                    if care == 'loss':
-                        return dict(
-                                monitor = 'val_{}'.format(classifier.metrics_names[0]),
-                                mode = 'min',
-                                patience = 3,
-                                min_delta = 1e-2)
-                    elif care == 'metric':
-                        return dict(
-                                monitor = 'val_{}'.format(classifier.metrics_names[-1]),
-                                mode = 'max',
-                                patience = 4,
-                                min_delta = 1e-4)
-                    else:
-                        print('why?')
-                callBackList = make_CallBackList(
-                       model_name,
-                       verbose          = 0,# print out the process
-                       frequency        = 1,
-                       **call_back_dict('metric')
-                       )
+                # prepare the data
+                X_train,y_train = utils_deep.prepare_data_batch(X_train,y_train,batch_size = batch_size)
+                X_valid,y_valid = utils_deep.prepare_data_batch(X_valid,y_valid,batch_size = batch_size)
+                # put weights on minor class
+                class_weight    = dict(Counter(y_train[:,0]))
+                class_weight    = {key:(y_train.shape[0] - value)/value for key,value in class_weight.items()}
+                sample_weight   = [class_weight[item] for item in y_train[:,0]]
+                print(class_weight)
                 
+                # train and validate
                 np.random.seed(12345)
                 X_train,y_train = shuffle(X_train,y_train)
-                
                 classifier.fit(X_train,y_train,
                                batch_size               = batch_size,
                                epochs                   = n_epochs,
@@ -274,23 +143,15 @@ for subject in all_subjects:
                                sample_weight            = np.array(sample_weight), # this is the key !
                                )
                 
-                
+                # test the trained model
                 classifier.load_weights(model_name)
                 
-                
-#                X_test,_,_ = preprocess_features(X[idx_test],vectorizer,scaler)
-                X_test = X[idx_test] / np.abs(X[idx_test].max())
-                remain_test = X_test.shape[0] % batch_size
-                if remain_test != 0:
-                    X_test,y_test = X_test[:-remain_test],targets[idx_test][:-remain_test]
-                else:
-                    y_test = targets[idx_test]
+                X_test,y_test = utils_deep.prepare_data_batch(X[idx_test],targets[idx_test],batch_size = batch_size)
                 
                 
                 preds = classifier.predict(X_test,batch_size=batch_size,verbose = 0)
                 
-                ss.append(metrics.roc_auc_score(y_test,
-                                                preds))
+                ss.append(metrics.roc_auc_score(y_test,preds,average = 'micro'))
                 print(f'{conscious_state},fold {fold+1},{ss[-1]:.4f}')
                 K.clear_session()
             results.append([conscious_state,ss])
--- scripts/EEG/RNN decoding test 2.py
+++ scripts/EEG/RNN decoding test 2.py
@@ -22,6 +22,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torch.autograd as autograd
 
 
 def preprocess_features(X,vectorizer = None,scaler = None):
@@ -40,35 +41,31 @@ def preprocess_features(X,vectorizer = None,scaler = None):
     return X_vec_tran,vectorizer,scaler
 
 
-all_subjects = [
-                'matie_5_23_2019',
-                'pedro_5_14_2019',
-                'aingere_5_16_2019',
-                'inaki_5_9_2019',
+all_subjects = ['aingere_5_16_2019',
+                'alba_6_10_2019',
+                'alvaro_5_16_2019',
                 'clara_5_22_2019',
                 'ana_5_21_2019',
+                'inaki_5_9_2019',
+                'jesica_6_7_2019',
                 'leyre_5_13_2019',
                 'lierni_5_20_2019',
-                'xabier_5_15_2019',
                 'maria_6_5_2019',
+                'matie_5_23_2019',
+                'out_7_19_2019',
+                'mattin_7_12_2019',
+                'pedro_5_14_2019',
+                'xabier_5_15_2019',
                 ]
 all_subjects = np.sort(all_subjects)
 
 torch.manual_seed(12345)
 for subject in all_subjects:
-    working_dir         = f'../../data/clean EEG/{subject}'
+    working_dir         = f'../../data/clean EEG highpass detrend/{subject}'
     working_data        = glob(os.path.join(working_dir,'*-epo.fif'))
-    # there was a bug in the csv file, so the early behavioral is treated differently
-    date                = '/'.join(re.findall('\d+',subject))
-    date                = datetime.strptime(date,'%m/%d/%Y')
-    breakPoint          = datetime(2019,3,10)
-    if date > breakPoint:
-        new             = True
-    else:
-        new             = False
-    frames,_            = get_frames(directory = f'../../data/behavioral/{subject}',new = new)
     n_splits            = 25
     n_epochs            = int(2e2)
+    print_model         = True
     
     
     for epoch_file in working_data:
@@ -91,30 +88,130 @@ for subject in all_subjects:
             epochs
             epochs = epochs.pick_types(eeg=True)
             
-            X_,y_               = epochs.get_data(),epochs.events[:,-1]
+            X_,y_               = epochs.crop(0,1.).get_data(),epochs.events[:,-1]
             y_                  = y_ //100 - 2
             
             
             X,targets           = X_.copy(),y_.copy()
+            targets             = np.vstack([targets,1-targets]).T
             
             X                   = mne.decoding.Scaler(epochs.info).fit_transform(X)
             
             X                   = np.swapaxes(X,1,2)
             ss                  = []
-            cv                  = StratifiedShuffleSplit(n_splits=n_splits,test_size = 0.2,random_state=12345)
+            cv                  = StratifiedShuffleSplit(n_splits=n_splits,test_size = 0.15,random_state=12345)
             for fold,(idx_,idx_test) in enumerate(cv.split(X,targets)):
                 
                 X_train,X_valid,y_train,y_valid = train_test_split(
                         X[idx_],targets[idx_],
-                        test_size               = 0.2,
+                        test_size               = 0.15,
                         random_state            = 12345,
                         shuffle                 = True,)
                 
-                X_train,vectorizer,scaler = preprocess_features(X_train)
-                X_valid,_,_ = preprocess_features(X_valid,vectorizer,scaler)
-
-
-
+#                X_train,vectorizer,scaler = preprocess_features(X_train)
+#                X_valid,_,_ = preprocess_features(X_valid,vectorizer,scaler)
+                X_train = X_train / np.abs(X_train.max())
+                X_valid = X_valid / np.abs(X_valid.max())
+                
+                time_step = X_train.shape[1]
+                n_features = X_train.shape[-1]
+                
+                class RNN_clf(nn.Module):
+                    def __init__(self,
+                                 batch_size,
+                                 n_layers = 1,
+                                 input_size = 60,
+                                 hidden_size = 32,
+                                 dropout = 0.5,
+                                 bidriectional = False
+                                 ):
+                        super(RNN_clf, self).__init__()
+                        self.input_size = input_size
+                        self.hidden_size = hidden_size
+                        self.num_layers = n_layers
+                        self.dropout = dropout
+                        self.bidriectional = bidriectional
+                        self.batch_size = batch_size
+                        self.rnn = nn.LSTM(input_size = self.input_size,
+                                           hidden_size = self.hidden_size,
+                                           num_layers = self.num_layers,
+                                           bias = True,
+                                           batch_first = True,
+                                           bidirectional = self.bidriectional,)
+                        if self.bidriectional:
+                            self.linear = nn.Linear(self.hidden_size*2,2)
+                            self.pool = nn.AdaptiveAvgPool2d(output_size = (1,self.hidden_size*2))
+                        else:
+                            self.linear = nn.Linear(self.hidden_size,2)
+                            self.pool = nn.AdaptiveAvgPool2d(output_size = (1,self.hidden_size))
+                        self.softmax = nn.LogSoftmax(dim = 1)
+#                        self.activation = nn.LeakyReLU(inplace = True)
+                        self.batchnormalization = nn.BatchNorm1d(self.hidden_size)
+                        self.hidden = self.init_hidden()
+                        self.exp = torch.exp
+                        self.squeeze = torch.squeeze
+                        
+                    def init_hidden(self):
+                        if self.bidriectional:
+                            return (autograd.Variable(torch.randn(self.num_layers*2, self.batch_size, self.hidden_size)),
+                                    autograd.Variable(torch.randn(self.num_layers*2, self.batch_size, self.hidden_size)))
+                        else:
+                            # This is what we'll initialise our hidden state as
+                            return (autograd.Variable(torch.randn(self.num_layers, self.batch_size, self.hidden_size)),
+                                    autograd.Variable(torch.randn(self.num_layers, self.batch_size, self.hidden_size)))
+                    def forward(self, x):
+                        lstm_out,self.hidden = self.rnn(x,self.hidden)
+                        lstm_out = self.batchnormalization(lstm_out)
+                        lstm_out = self.squeeze(self.pool(lstm_out))
+                        y_pred = self.linear(lstm_out)
+                        y_pred = self.softmax(y_pred)
+#                        y_pred = self.exp(self.softmax(y_pred))
+#                        y_pred = self.squeeze(y_pred)[-1]
+                        return y_pred
+                
+                batch_size = 1
+                model = RNN_clf(batch_size = batch_size,
+                                n_layers = 1,
+                                input_size = n_features,
+                                hidden_size = time_step,
+                                bidriectional=True)
+                model
+                optimizer = optim.Adam(model.parameters(),lr = 1e-3,)
+                loss_fn = nn.CrossEntropyLoss()
+                train_loss = 0.
+                
+                for ii,(x,y) in enumerate(zip(X_train,y_train)):
+                    if batch_size > 1:
+                        x = x.astype('float32')
+                    else:
+                        x = x.reshape(-1,time_step,n_features).astype('float32')
+                        y = y.reshape(-1,2)
+#                    y = np.vstack([y for _ in range(time_step)]).reshape(-1,time_step,2).astype('float32')
+                    
+                    x = autograd.Variable(torch.from_numpy(x))
+                    y = autograd.Variable(torch.from_numpy(y).type(torch.LongTensor))
+                    
+                    optimizer.zero_grad()
+                    output = model(x)
+                    loss = loss_fn(output.view(-1)[:,np.newaxis],y.view(-1)[:,np.newaxis])
+                    loss.backward(retain_graph=True)
+                    optimizer.step()
+                    
+                    train_loss += loss.data
+                    
+                    if ii % 50 == 0:
+                        with torch.no_grad():
+                            valid_output = torch.squeeze(torch.stack([model(autograd.Variable(torch.from_numpy(temp.reshape(-1,time_step,n_features).astype('float32')))) for temp in X_valid]))
+                            lo = loss_fn(valid_output,
+                                         autograd.Variable(torch.from_numpy(y_valid).type(torch.LongTensor)))
+                            print(ii,lo.data)
+                
+                X_test = X[idx_test] / np.abs(X[idx_test].max())
+                y_test = targets[idx_test]
+                
+                with torch.no_grad():
+                    test_output = torch.squeeze(torch.stack([model(autograd.Variable(torch.from_numpy(temp.reshape(-1,time_step,n_features).astype('float32')))) for temp in X_test]))
+                sadf
 
 
 
