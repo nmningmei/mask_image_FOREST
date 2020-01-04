8fa417913ffd4f9afe59729a8b89fa9e9b4ff05a
Ning Mei <semsociaty@gmail.com>
2020-01-04 13:59:10 +0100

    Add files via upload

--- scripts/MRI/nilearn_workflow/get computer vision features.py
+++ scripts/MRI/nilearn_workflow/get computer vision features.py
@@ -16,6 +16,7 @@ from torch.utils.data import DataLoader
 from torch.autograd import Variable
 from torch import nn
 import numpy as np
+from sklearn.metrics import roc_auc_score
 
 class ImageFolderWithPaths(datasets.ImageFolder):
     """Custom dataset that includes image file paths. Extends
@@ -32,76 +33,104 @@ class ImageFolderWithPaths(datasets.ImageFolder):
         tuple_with_path = (original_tuple + (path,))
         return tuple_with_path
 
-working_dir = '../../../data/bw_bc_bl'
-working_data = glob(os.path.join(working_dir,
+working_dirs = ['../../../data/greyscaled','../../../data/experiment_images_grayscaled']
+working_data = glob(os.path.join(working_dirs[0],
                                  "*",
                                  "*",
                                  "*.jpg"))
 
 
-transform = transforms.Compose([            #[1]
-# transforms.Resize(256),                    #[2]
-# transforms.CenterCrop(224),                #[3]
-# transforms.Grayscale(),
- transforms.ToTensor(),                     #[4]
- transforms.Normalize(                      #[5]
-                         mean=[0.485],                #[6]
-                         std=[0.229]                  #[7]
+transform = transforms.Compose([
+ transforms.Resize(128),
+ transforms.RandomRotation(45),
+ transforms.RandomHorizontalFlip(),
+ transforms.RandomVerticalFlip(),
+ transforms.ToTensor(),
+ transforms.Normalize(
+                         mean=[0.485],
+                         std=[0.229]
  )])
 
 
 
 
 picked_models = dict(
-        resnet18 = models.resnet18(pretrained=True),
-        alexnet = models.alexnet(pretrained=True),
-        squeezenet = models.squeezenet1_1(pretrained=True),
-        vgg19_bn = models.vgg19_bn(pretrained=True),
-        densenet121 = models.densenet121(pretrained=True),
+#        resnet18 = models.resnet18(pretrained=True),
+#        alexnet = models.alexnet(pretrained=True),
+#        squeezenet = models.squeezenet1_1(pretrained=True),
+#        vgg19_bn = models.vgg19_bn(pretrained=True),
+        densenet169 = models.densenet169(pretrained=True),
 #        inception = models.inception_v3(pretrained=True),
-        googlenet = models.googlenet(pretrained=True),
-        shufflenet = models.shufflenet_v2_x0_5(pretrained=True),
-        mobilenet = models.mobilenet_v2(pretrained=True),
-        resnext50_32x4d = models.resnext50_32x4d(pretrained=True),
+#        googlenet = models.googlenet(pretrained=True),
+#        shufflenet = models.shufflenet_v2_x0_5(pretrained=True),
+#        mobilenet = models.mobilenet_v2(pretrained=True),
+#        resnext50_32x4d = models.resnext50_32x4d(pretrained=True),
         )
-def train_model(model_working,working_dir,transform,batch_size = 8):
+def load_dataset(working_dir,transform,batch_size = 1,num_workers = 1,shuffle = True):
+    dataset_from_folder = datasets.ImageFolder(
+        root=working_dir,
+        transform=transform)
+    data_loader = torch.utils.data.DataLoader(
+        dataset_from_folder,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=shuffle,
+    )
+    return data_loader
+def train_model(model_working,working_dirs,transform,n_epoch,device = 'cpu',batch_size = 8):
     
-    ImageLoader = ImageFolderWithPaths(working_dir,transform=transform,)
-    training_iterator = iter(DataLoader(ImageLoader,
-                                    batch_size = batch_size,
-                                    num_workers = 1,
-                                    shuffle = True))
-    model_working.eval()
-    model_working = model_working#.cuda()
-    criterion = nn.NLLLoss()
-    optimizer = torch.optim.Adam(model_loaded.parameters(),
+    ImageLoader_train = load_dataset(working_dirs[0],transform,batch_size = batch_size,num_workers = 2,shuffle = True)
+    ImageLoader_valid = load_dataset(working_dirs[1],transform,batch_size = batch_size,num_workers = 2,shuffle = True)
+    train_iterator = iter(ImageLoader_train)
+    valid_iterator = iter(ImageLoader_valid)
+    
+    model_working = model_working.to(device)
+    criterion = nn.BCEWithLogitsLoss()
+#    print(list(model_working.parameters()))
+    optimizer = torch.optim.Adam(model_working.parameters(),
                                  lr = 0.001, 
                                  weight_decay = 1e-4)
     torch.manual_seed(12345)
     np.random.seed(12345)
-    for ii, (batch_in,batch_label,_) in enumerate(training_iterator):
+    model_working.train()
+    for ii, (batch_in,batch_label) in enumerate(train_iterator):
 #        print(batch_label)
         running_loss = 0.
-        batch_in = Variable(batch_in,requires_grad = False)#.cuda()
-        batch_label = Variable(batch_label,requires_grad = False)#.cuda()
+        batch_in = Variable(batch_in.float()).to(device)
+        batch_label = torch.stack([1 - batch_label,batch_label]).T
+        batch_label = Variable(batch_label).to(device)
         
         optimizer.zero_grad()
         
         preds = model_working(batch_in)
-        loss = criterion(preds,batch_label)
+        loss = criterion(preds,Variable(batch_label.float()))
         loss.backward()
         optimizer.step()
         
         running_loss += loss.item()
-        if ii % 50 == 0:
-            print('step {:3},loss = {:.4f}'.format(ii+1,running_loss/(ii+1)))
-    print('finish training, loss = {:.4f}'.format(running_loss/(ii+1)))
+        print(f'step {n_epoch}-{(ii+1)/len(train_iterator):.3f},loss = {running_loss/(ii+1):.4f}')
+    valid_loss = 0.
+    y_pred = []
+    y_true = []
+    for jj, (batch_in,batch_label) in tqdm(enumerate(valid_iterator),desc='validation'):
+        with torch.no_grad():
+            model_working.eval()
+            batch_in = Variable(batch_in.float()).to(device)
+            batch_label = torch.stack([1 - batch_label,batch_label]).T
+            batch_label = Variable(batch_label).to(device)
+            preds = model_working(batch_in)
+            y_pred.append(preds)
+            y_true.append(batch_label)
+            loss = criterion(preds,Variable(batch_label.float()))
+            valid_loss += loss.item()
+    y_pred,y_true = torch.cat(y_pred).detach().cpu().numpy(),torch.cat(y_true).detach().cpu().numpy()
+    score_ = roc_auc_score(y_true.astype(int),y_pred.astype('float32'))
+    print(f'epoch {n_epoch}, validation loss = {valid_loss/(jj+1):.4f}, score = {score_:.4f}')
     
-    return model_working
-def feature_extract(feature_extractor,model_name,working_dir,transform):
-    ImageLoader = ImageFolderWithPaths(working_dir,transform=transform,)
-    data = DataLoader(ImageLoader,batch_size=1,shuffle = False)
-    data_iterator = iter(data)
+    return model_working,score_
+def feature_extract(feature_extractor,model_name,working_dir,transform,batch_size = 8):
+    ImageLoader = load_dataset(working_dir,transform,batch_size = batch_size,num_workers = 2,shuffle = True)
+    data_iterator = iter(ImageLoader)
     
     for batch_in,_ ,paths in tqdm(data_iterator,desc='{}'.format(model_name)):
         is_train = False
@@ -116,6 +145,7 @@ def feature_extract(feature_extractor,model_name,working_dir,transform):
 
 for model_name,model_loaded in picked_models.items():
     torch.cuda.empty_cache()
+    
     if not os.path.exists(f'../../../data/computer vision features/{model_name}'):
         os.makedirs(f'../../../data/computer vision features/{model_name}')
     model_working = deepcopy(model_loaded)
@@ -123,105 +153,19 @@ for model_name,model_loaded in picked_models.items():
         param.requires_grad = False
     print(model_name)
     # training
-    if (model_name == 'resnet18') or (model_name == 'googlenet') or (model_name == 'shufflenet') or (model_name == 'resnext50_32x4d'):
-        model_working.fc = nn.Sequential(*[
-                            nn.Dropout(),
-                            nn.Linear(in_features = model_loaded.fc.in_features,
-                                      out_features = 4096,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Dropout(),
-                            nn.Linear(in_features = 4096,
-                                      out_features = 1024,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Dropout(),
-                            nn.Linear(in_features = 1024,
-                                      out_features = 512,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Linear(in_features = 512,
-                                      out_features = 2,bias = True,),
-                            nn.LogSoftmax(dim = 1),
-                ])
-    elif (model_name == 'alexnet') or (model_name == 'mobilenet') :
-        model_working.classifier = nn.Sequential(*[
-                            nn.Dropout(),
-                            nn.Linear(in_features = model_loaded.classifier[1].in_features,
-                                      out_features = 4096,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Dropout(),
-                            nn.Linear(in_features = 4096,
-                                      out_features = 1024,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Dropout(),
-                            nn.Linear(in_features = 1024,
-                                      out_features = 512,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Linear(in_features = 512,
-                                      out_features = 2,bias = True,),
-                            nn.LogSoftmax(dim = 1),
-                ])
-    elif model_name == 'vgg19_bn':
-        model_working.classifier = nn.Sequential(*[
-                            nn.Dropout(),
-                            nn.Linear(in_features = model_loaded.classifier[0].in_features,
-                                      out_features = 4096,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Dropout(),
-                            nn.Linear(in_features = 4096,
-                                      out_features = 1024,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Dropout(),
-                            nn.Linear(in_features = 1024,
-                                      out_features = 512,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Linear(in_features = 512,
-                                      out_features = 2,bias = True,),
-                            nn.LogSoftmax(dim = 1),
-                ])
-    elif model_name == 'densenet121':
-        model_working.classifier = nn.Sequential(*[
-                            nn.Dropout(),
-                            nn.Linear(in_features = model_loaded.classifier.in_features,
-                                      out_features = 4096,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Dropout(),
-                            nn.Linear(in_features = 4096,
-                                      out_features = 1024,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Dropout(),
-                            nn.Linear(in_features = 1024,
-                                      out_features = 512,bias = True),
-                            nn.ReLU(inplace = True,),
-                            nn.Linear(in_features = 512,
-                                      out_features = 2,bias = True,),
-                            nn.LogSoftmax(dim = 1),
-                ])
-    elif model_name == 'squeezenet':
-        model_working.classifier = nn.Sequential(*[
-                nn.Dropout(p=0.5, inplace=False),
-                nn.Conv2d(512, 2, kernel_size=(1, 1), stride=(1, 1)),
-                nn.ReLU(inplace=True),
-                nn.AdaptiveAvgPool2d(output_size=(1, 1)),
-                nn.LogSoftmax(dim = 1)])
-    model_trained = train_model(model_working,working_dir,transform,batch_size = 8)
-    
-    # feature extraction
-    
-    if (model_name == 'resnet18') or (model_name == 'googlenet')\
-     or (model_name == 'shufflenet') or (model_name == 'resnext50_32x4d'):
-        feature_extractor = deepcopy(model_trained)
-        seqs = list(model_trained.fc.children())[:-3]
-        seqs.append(nn.Sigmoid())
-        feature_extractor.fc = nn.Sequential(*seqs)
-    elif (model_name == 'alexnet') or (model_name == 'mobilenet')\
-    or (model_name == 'vgg19_bn') or(model_name == 'densenet121'):
-        feature_extractor = deepcopy(model_trained)
-        seqs = list(model_trained.classifier.children())[:-3]
-        seqs.append(nn.Sigmoid())
-        feature_extractor.classifier = nn.Sequential(*seqs)
-    elif model_name == 'squeezenet':
-        feature_extractor = nn.Sequential(*[model_trained.features,
-                                            nn.AdaptiveAvgPool2d(output_size = (1,1))])
-    feature_extract(feature_extractor,model_name,working_dir,transform)
+    model_working.classifier = nn.Sequential(*[
+                        nn.Linear(in_features = model_loaded.classifier.in_features,
+                                  out_features = 300,bias = True),
+                        nn.ReLU(inplace = True,),
+                        nn.Linear(in_features = 300,
+                                  out_features = 2,bias = True,),
+                        nn.Softmax(dim = 1),
+            ])
+    batch_size = 8
+    for n_epochs in range(10):
+        model_trained,score_ = train_model(model_working,working_dirs,transform,n_epochs,batch_size = batch_size)
+    adf
+    feature_extract(feature_extractor,model_name,working_dirs[0],transform)
 
 
 
